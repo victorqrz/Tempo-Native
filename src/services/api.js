6653d9f7ef6e991211a6ba6f1ff0820400d8b1c1
victorqrz <victoorqrz@gmail.com>
2021-06-08 09:44:31 -0300

    updating

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 //https://api.hgbrasil.com/weather?key=b037621d&lat=-23.682&lon=-46.875
 
-export const key = 'b037621d';
+export const key = 'c107e9c1';
 
 const api = axios.create({
   baseURL: 'https://api.hgbrasil.com'
